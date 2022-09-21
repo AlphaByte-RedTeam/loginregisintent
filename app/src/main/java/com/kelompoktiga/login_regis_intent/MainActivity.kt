daab80918c3b548624466fe160dc14302ee7c7b3
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2022-09-21 23:15:08 +0700

    feat: add splash screen

--- app/src/main/java/com/kelompoktiga/login_regis_intent/MainActivity.kt
+++ app/src/main/java/com/kelompoktiga/login_regis_intent/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.kelompoktiga.login_regis_intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContentView(R.layout.activity_main)
     }
 }
