7344defc9af0b231f8d936f382e0b94e7b14b599
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-09-24 11:53:34 +0700

    feat: make continue button direct to login activity

--- app/src/main/java/com/kelompoktiga/login_regis_intent/MainActivity.kt
+++ app/src/main/java/com/kelompoktiga/login_regis_intent/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.kelompoktiga.login_regis_intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,11 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
 
         setContentView(R.layout.activity_main)
+
+        val continueButton: Button = findViewById(R.id.btnContinue)
+        continueButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
