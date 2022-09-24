d57eb20b0e9ac8111e6fd06477492537f6fd3a86
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-09-24 11:37:47 +0700

    chore: remove unused import

--- app/src/main/java/com/kelompoktiga/login_regis_intent/MainActivity.kt
+++ app/src/main/java/com/kelompoktiga/login_regis_intent/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.kelompoktiga.login_regis_intent
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.WindowCompat
-import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
