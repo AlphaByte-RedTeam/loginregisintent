790b75dae3dd53dafc6dc766833af636d29824d5
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-09-24 18:39:48 +0700

    feat: make the login footer to clickable

--- app/src/main/java/com/kelompoktiga/login_regis_intent/RegisterActivity.kt
+++ app/src/main/java/com/kelompoktiga/login_regis_intent/RegisterActivity.kt
@@ -1,11 +1,47 @@
 package com.kelompoktiga.login_regis_intent
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.view.View
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        
+        val tvFooter: TextView = findViewById(R.id.txtFooterRegis)
+
+        val foregroundColorSpan = ForegroundColorSpan(getColor(R.color.pickled_bluewood))
+        val boldSpan = StyleSpan(android.graphics.Typeface.BOLD)
+
+        val spannableString = SpannableString(tvFooter.text.toString())
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun updateDrawState(drawState: TextPaint) {
+                super.updateDrawState(drawState)
+                drawState.isUnderlineText = true
+            }
+        }
+        spannableString.setSpan(boldSpan, 25, 30, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(foregroundColorSpan, 25, 30, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(clickableSpan, 25, spannableString.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        tvFooter.text = spannableString
+        tvFooter.movementMethod = LinkMovementMethod.getInstance()
     }
 }
