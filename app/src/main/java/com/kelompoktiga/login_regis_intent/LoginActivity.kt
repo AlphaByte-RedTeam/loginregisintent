ad721703cafe6379d1b7818cd8666d4dff7f24bc
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-09-25 16:45:54 +0700

    feat: add google sso to login activity

--- app/src/main/java/com/kelompoktiga/login_regis_intent/LoginActivity.kt
+++ app/src/main/java/com/kelompoktiga/login_regis_intent/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.kelompoktiga.login_regis_intent
 
 import android.content.Intent
+import android.content.IntentSender
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,10 +12,17 @@ import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -32,6 +40,13 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var email: String
     private lateinit var password: String
 
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+    private lateinit var oneTapClient: SignInClient
+
+    private val REQ_ONE_TAP = 2 // can be any integer unique to the activity
+    private var showOneTapUI = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -41,6 +56,46 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
         etEmail = findViewById(R.id.editEmailLogin)
         etPassword = findViewById(R.id.editPasswordLogin)
+
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                .setSupported(true)
+                .build())
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(getString(R.string.default_web_client_id))
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            .setAutoSelectEnabled(true)
+            .build()
+
+        oneTapClient = Identity.getSignInClient(this)
+        oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender,
+                        REQ_ONE_TAP,
+                        null,
+                        0,
+                        0,
+                        0,
+                        null
+                    )
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(this) { e ->
+                // No saved credentials found. Launch the One Tap sign-up flow,
+                // or do nothing and continue presenting the signed-out UI
+
+                // See: https://developers.google.com/identity/one-tap/android/create-new-accounts?authuser=0
+                // for further details on how to implement One Tap sign-up.
+                // val inten: Intent = Intent(this, RegisterActivity::class.java)
+                Log.e("OneTap", "Error getting pending intent", e)
+            }
 
         btnLogin = findViewById(R.id.btnLogin)
         btnLogin.setOnClickListener() {
@@ -60,6 +115,11 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                         }
                     }
             }
+        }
+
+        btnGoogleLogin = findViewById(R.id.btnGoogleLogin)
+        btnGoogleLogin.setOnClickListener() {
+            signIn()
         }
 
         val foregroundColorSpan = ForegroundColorSpan(getColor(R.color.pickled_bluewood))
@@ -98,7 +158,57 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
         tvFooter.text = spannableString
         tvFooter.movementMethod = LinkMovementMethod.getInstance()
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            @Suppress("DEPRECATION")
+            super.onActivityResult(requestCode, resultCode, data)
+
+            when (requestCode) {
+                REQ_ONE_TAP -> {
+                    try {
+                        val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                        val idToken = credential.googleIdToken
+                        val username = credential.id
+                        val password = credential.password
+                        when {
+                            idToken != null -> {
+                                // Got an ID token from Google. Use it to authenticate with Firebase
+                                Log.d(TAG, "Got ID token")
+                            }
+                            password != null -> {
+                                // Got a saved username and password. Use them to authenticate with Firebase
+                                Log.d(TAG, "Got password")
+                            }
+                            else -> {
+                                // Shouldn't happen
+                                Log.d(TAG, "No ID token!")
+                            }
+                        }
+                    } catch (e: ApiException) {
+                        when (e.statusCode) {
+                            CommonStatusCodes.CANCELED -> {
+                                Log.d(TAG, "One-tap dialog was closed.")
+                                // Don't re-prompt the user
+                                showOneTapUI = false
+                            }
+                            CommonStatusCodes.NETWORK_ERROR -> {
+                                Log.d(TAG, "One-tap encountered a network error.")
+                                // Try again or just ignore
+                            }
+                            else -> {
+                                Log.d(TAG, "Couldn't get credential from result: ${e.localizedMessage}")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    private fun signIn() {
+        // TODO("Implement sign in method in android studio kotlin using Firebase auth")
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
     override fun onStart() {
@@ -150,5 +260,10 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         // TODO: create input validation
 
         return true
+    }
+
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
     }
 }
